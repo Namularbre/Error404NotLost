9167e1898177bb1d9c887ba51bb10b47c9b38fe1
Namu <namulabre@gmail.com>
2025-07-28 14:54:06 +0200

    Ajoutez des fichiers projet.

---
+++ Error404NotLost_DAL/Error404NotFoundDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Error404NotLost_DAL
+{
+    public class Error404NotFoundDbContext : IdentityDbContext
+    {
+        public Error404NotFoundDbContext(DbContextOptions<Error404NotFoundDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Error404NotLost_WEBASP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Error404NotLost_WEBASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
